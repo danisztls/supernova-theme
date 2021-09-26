8257a9a3678d633c132e00b5bcdb83aa2e7db32b
Daniel Souza <me@posix.dev.br>
2021-09-25 22:15:46 -0300

    Improve testing suite

---
+++ test/TESTING.md
@@ -0,0 +1,6 @@
+# Testing
+This is a collection of tools and tests to check how the theme looks on the terminal.
+
+The shell script `test.sh` prints 256 terminal colors and run the tests when invoked.
+
+Under `sample/` there are reference blank files for testing ls/[exa](https://the.exa.website/).
--- test/test.sh
+++ test/test.sh
@@ -1,23 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Print terminal colors for testing
-printColor() {
-    printf "$(tput setaf "$1"; tput setab "$1")   $(tput op) %-4s" "$1"
+
+# localPath="$(sed -E 's/(.*\/).*.sh/\1/' <<< $0)"
+localPath=${0%test.sh}
+
+_printColor() {
+  printf "$(tput setaf "$1"; tput setab "$1")   $(tput op) %-4s" "$1"
 }
 
-print256Pallete() {
-    # bash ranges don't support variables
-    # can't do 'for i in {$s..$e}; do'
-    for color in {0..7}; do
-        printColor "$color"
-    done; printf "\n\n"
+256Pallete() {
+  # bash ranges don't support variables
+  # can't do 'for i in {$s..$e}; do'
+  for color in {0..7}; do
+    _printColor "$color"
+  done; printf "\n\n"
 
-    for color in {8..15}; do
-        printColor "$color"
-    done; printf "\n\n"
+  for color in {8..15}; do
+    _printColor "$color"
+  done; printf "\n\n"
 
-    for color in {16..255}; do
-        printColor "$color"
-    done; printf "\n\n"
+  for color in {16..255}; do
+    _printColor "$color"
+  done; printf "\n\n"
 }
 
-print256Pallete
+256Pallete
+
+filesList() {
+  if command -v exa >/dev/null; then
+    exa --long "$localPath/sample"
+  else
+    ls "$localPath/sample"
+  fi
+} 
+
+filesList
